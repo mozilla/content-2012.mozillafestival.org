7559977c6d683c6768cb1336bc00255d32b93eef
Andrew Hayward <github@andrewhayward.net>
2012-08-21 22:47:40 +0100

    Putting WP hooks into header and footer

--- wp-content/themes/mozfest2012/footer.php
+++ wp-content/themes/mozfest2012/footer.php
@@ -7,5 +7,6 @@
 				?>
 			</div>
 		</div>
+		<?php wp_footer(); ?>
 	</body>
 </html>
--- wp-content/themes/mozfest2012/header.php
+++ wp-content/themes/mozfest2012/header.php
@@ -5,8 +5,9 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title></title>
 		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/media/css/core.css">
+		<?php wp_head(); ?>
 	</head>
-	<body>
+	<body <?php body_class(); ?>>
 		<div id="header">
 			<div class="constrained">
 				<header>
