ac54a2982cce2403374fd03b11afdcb358380931
Andrew Hayward <github@andrewhayward.net>
2012-09-05 13:54:05 +0100

    Removing microformats from taxonomy list
    Not entirely sure how that ended up in there

--- wp-content/themes/mozfest2012/taxonomy-list.php
+++ wp-content/themes/mozfest2012/taxonomy-list.php
@@ -7,9 +7,8 @@ $terms = get_terms(TAXONOMY_ARCHIVE, array('orderby'=>'term_group', 'hide_empty'
 echo '<h2>' . $taxonomy->labels->name . '</h2>';
 
 foreach ($terms as $term) {
-	$child = $term->parent ? ' child' : '';
-	echo '<article class="vcard' . $child . '">';
-	echo '<h3><a href="' . get_term_link($term) . '" class="fn url">' . $term->name . '</a></h3>';
+	echo '<article>';
+	echo '<h3><a href="' . get_term_link($term) . '">' . $term->name . '</a></h3>';
 	echo '<p>' . $term->description . '</p>';
 	echo '</article>';
 }
