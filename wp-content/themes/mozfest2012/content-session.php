56b33a0c1d8dfabc539d8a1e75c2448f7d443c2f
Andrew Hayward <github@andrewhayward.net>
2012-09-24 18:06:09 +0100

    Adding some share buttons to sessions

--- wp-content/themes/mozfest2012/content-session.php
+++ wp-content/themes/mozfest2012/content-session.php
@@ -118,6 +118,28 @@
 		?>
 	<aside class="share">
 		<h3>Share this session</h3>
+		<ul>
+			<li>
+				<a href="https://twitter.com/share" class="twitter-share-button" data-hashtags="mozfest">Tweet</a>
+				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+			</li>
+			<script>
+				(function() {
+					var url = document.location,
+					    width = 107,
+					    src = '//www.facebook.com/plugins/like.php?href='+url+'&amp;send=false&amp;layout=button_count&amp;width='+width+'&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font&amp;height=21';
+					document.write('<li><iframe src="'+src+'" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:'+width+'px; height:21px;" allowTransparency="true"></iframe></li>');
+				})();
+			</script>
+			<script type="text/javascript">
+				(function() {
+					document.write('<li><div class="g-plus" data-action="share" data-annotation="bubble"></div></li>')
+					var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+					po.src = 'https://apis.google.com/js/plusone.js';
+					var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+				})();
+			</script>
+		</ul>
 	</aside>
 	<?php } ?>
 </article>
