6f306b8a8488dbcac2361698e226cad599cabe4f
Ross Bruniges <rossbruniges@gmail.com>
2012-10-31 14:52:42 +0000

    [Lighthouse-60] Linking through to individual blog posts on the blog homepage

--- wp-content/themes/mozfest2012/content.php
+++ wp-content/themes/mozfest2012/content.php
@@ -1,8 +1,8 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php if (is_single()): ?>
-		<h2><?php the_title(); ?></h2>
-	<?php elseif (!is_page()): ?>
-		<h3><?php the_title(); ?></h3>
-	<?php endif; ?>
-	<?php the_content(); ?>
+    <?php if (is_single()): ?>
+        <h2><?php the_title(); ?></h2>
+    <?php elseif (!is_page()): ?>
+        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+    <?php endif; ?>
+    <?php the_content(); ?>
 </article>
