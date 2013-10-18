b647c82119af1b443f3fa40180ef60299c1f20bb
Andrew Hayward <github@andrewhayward.net>
2013-10-18 18:02:33 +0100

    Fixing blog styling

--- wp-content/themes/mozfest2012/content.php
+++ wp-content/themes/mozfest2012/content.php
@@ -1,11 +1,20 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <?php if (is_single()): ?>
-        <h2><?php the_title(); ?></h2>
+        <div class="panel">
+            <div class="header"><h2><?php the_title(); ?></h2></div>
     <?php elseif (!is_page()): ?>
-        <h3 class="blog-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+        <div class="panel">
+            <div class="header"><h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3></div>
     <?php endif; ?>
     <?php the_content(); ?>
     <?php if (is_single()): ?>
-        <?php the_tags('<p style="display: none;"><strong>Tagged:</strong> ', ', ', '</p>'); ?>
+        <?php if (has_tag()): ?>
+            <div class="footer">
+                <?php the_tags('<p><strong>Tagged:</strong> ', ', ', '</p>'); ?>
+            </div>
+        <?php endif; ?>
+        </div>
+    <?php elseif (!is_page()): ?>
+        </div>
     <?php endif; ?>
 </article>
